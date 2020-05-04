fb250358c196216151aa0f06e192ab155798e0e5
zhsss <3476832067@qq,com>
2020-05-04 17:39:21 +0800

    test

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+for file in ./*.sh;do
+  if [[ $file =~ $0 ]];then
+    continue
+  fi
+  printf "===================== %s ====================\n" "$file"
+  bash "$file"
+done
