ad155bf9962f8379e7dd0b924d54650623f13dfb
zhsss <48230390+zhsss@users.noreply.github.com>
2020-05-08 21:21:57 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-for file in ./*.sh;do
-  if [[ $file =~ $0 ]];then
-    continue
-  fi
-  printf "===================== %s ====================\n" "$file"
-  bash "$file"
-done
+echo "-------------------------------------------------------------------"
+grep -oP '^(\w+\.)+[A_Za-z]+' web_log.tsv | sort -n  | uniq -c | sort -nr -k1 | head -10
+echo "-------------------------------------------------------------------"
+grep -oP '(\d+\.){3}\d+\s' web_log.tsv | sort -nr | uniq -c | sort -nr -k1 | head -10
+echo "-------------------------------------------------------------------"
+grep -oP '(\/[^\s]+)+' web_log.tsv | sort -n  | uniq -c | sort -nr -k1 | head -10
+echo "-------------------------------------------------------------------"
+grep -oP '\s+\d{3}\s+(?=\d+)' web_log.tsv | sort | uniq -c | sort -k1 -nr | awk  '{array[$2]=$1; sum+=$1} END { for (i in array) printf "%-20s %-15d %6.6f%%\n", i, array[i], array[i]/sum*100}'
+echo "-------------------------------------------------------------------"
+grep -oP '\s+4\d{2}\s+(?=\d+)' web_log.tsv | sort -u | xargs -i sh -c "grep  -P '\s+{}(?=\d+)'  web_log.tsv  | awk '{printf \"%s %d \n\",\$5,\$6 }'| sort | uniq -c | sort -nr -k1 |head -10"
+echo "-------------------------------------------------------------------"
+grep -P $1'\s+' web_log.tsv | awk '{printf "%s \n",$1}'| sort | uniq -c | sort -nr -k1 | head -10
+echo
